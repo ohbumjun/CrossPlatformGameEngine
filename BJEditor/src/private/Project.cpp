320c00eb6e007f9598957d09f77fae199858c72f
ohbumjun <beomjun0638@hanmail.net>
2024-06-30 18:51:31 +0900

    feat(Editor) Create Basic Project Class Layout

--- BJEditor/src/private/Project.cpp
+++ BJEditor/src/private/Project.cpp
@@ -1,4 +1,11 @@
 ﻿#include "Project.h"
+#include "Hazel/Core/Serialization/JsonSerializer.h"
+#include "EditorContext.h"
+#include "ProjectContext.h"
+#include "Hazel/FileSystem/FileMemory.h"
+#include "Hazel/FileSystem/DirectorySystem.h"
+#include "Hazel/FileSystem/FileModes.h"
+#include "Panel/PanelController.h"
 
 namespace HazelEditor
 {
@@ -11,7 +18,7 @@ namespace HazelEditor
 
 	bool Project::IsLoaded()
 	{
-        return nullptr != lv_project_get_context();
+        return nullptr != BJ_GetProjectContext();
 	}
     void Project::Open(const char *projectAbsolutePath)
     {
@@ -37,90 +44,91 @@ namespace HazelEditor
 
     void Project::SaveSettings()
     {
-        const LvEngineContext *engine = lv_engine_get_context();
-        LvProjectSettings &settings = lv_project_get_context()->settings;
-
-        if (LvString::IsNullOrEmpty(settings.windowLayoutFilePath))
-        {
-            settings.windowLayoutFilePath =
-                LvPanelController::defaultDockingLayoutPath;
-        }
-
-        LvString settingJsonString;
-        LvJsonDomArchive archive;
-
-        const LvString directory = lv_path_directory(_settingsFilePath.c_str());
-        if (!lv_directory_exist(directory.c_str()))
-        {
-            lv_directory_create(directory.c_str());
-        }
-
-        LvFileStream settingsFile(_settingsFilePath.c_str(),
-                                  LvFileMode::CREATE);
-        LvOutputStream outputStream(&settingsFile);
-
-        settings.appleDevelopmentTeamId =
-            lv_editor_get_context()->settings.appleDevelopmentTeamId;
-
-        settings.major = engine->version.major;
-        settings.minor = engine->version.minor;
-        settings.patch = engine->version.patch;
-#ifndef _DEBUG
-        settings.build = engine->version.build;
-#endif
-
-        archive.Serialize(LvReflection::GetTypeId<LvProjectSettings>(),
-                          &settings);
-        settingJsonString = archive.GetResult();
-        outputStream.WriteText(settingJsonString.c_str());
-
-        settingsFile.Flush();
-        settingsFile.Close();
+        // const LvEngineContext *engine = lv_engine_get_context();
+        // LvProjectSettings &settings = BJ_GetProjectContext()->settings;
+        // 
+        // if (settings.windowLayoutFilePath.empty())
+        // {
+        //     settings.windowLayoutFilePath =
+        //         PanelController::defaultDockingLayoutPath;
+        // }
+        // 
+        // std::string settingJsonString;
+        // Hazel::JsonSerializer archive;
+        // 
+        // const std::string directory = lv_path_directory(_settingsFilePath.c_str());
+        // if (!Hazel::DirectorySystem::ExistDirectoryPath(directory.c_str()))
+        // {
+        //     Hazel::DirectorySystem::CreateDirectoryPath(directory.c_str());
+        // }
+        // 
+        // Hazel::FileMemory settingsFile(_settingsFilePath.c_str(),
+        //                                Hazel::FileOpenMode::CREATE);
+        // LvOutputStream outputStream(&settingsFile);
+        // 
+        // settings.appleDevelopmentTeamId =
+        //     BJ_GetProjectContext()->settings.appleDevelopmentTeamId;
+        // 
+        // settings.major = engine->version.major;
+        // settings.minor = engine->version.minor;
+        // settings.patch = engine->version.patch;
+        // 
+        // archive.Serialize(Hazel::Reflection::GetTypeID<LvProjectSettings>(),
+        //                   &settings);
+        // settingJsonString = archive.GetResult();
+        // outputStream.WriteText(settingJsonString.c_str());
+        // 
+        // settingsFile.FlushToFile();
+        // settingsFile.End();
     }
     void Project::LoadSettings()
     {
-        LvFileStream settingsFile(_settingsFilePath.c_str(), LvFileMode::OPEN);
-        LvInputStream inputStream(&settingsFile);
-
-        const LvString settingJsonString = inputStream.ReadToEnd();
-
-        settingsFile.Flush();
-        settingsFile.Close();
-
-        LvProjectContext *project = lv_project_get_context();
-
-        LvProjectSettings &settings = project->settings;
-
-        LvJsonDomArchive archive(settingJsonString.c_str());
-        archive.Deserialize(LvReflection::GetTypeId<LvProjectSettings>(),
-                            &settings);
-
-        LvEngineContext *engineContext = lv_engine_get_context();
-        engineContext->sceneGraphPath = settings.defaultSceneGraphFileName;
-
-        if (LvString::IsNullOrEmpty(settings.windowLayoutFilePath))
-        {
-            settings.windowLayoutFilePath =
-                LvPanelController::defaultDockingLayoutPath;
-        }
-
-        engineContext->isGPUskinning = settings.isGPUskinning;
-        if (settings.useHDR)
-        {
-            engineContext->colorRenderTextureFormat =
-                Render::LvPixelFormat::R16G16B16A16_SFLOAT;
-        }
-        else
-        {
-            engineContext->colorRenderTextureFormat =
-                Render::LvPixelFormat::R8G8B8A8_UNORM;
-        }
-        engineContext->debugWireFrame = settings.debugWireFrame;
-        engineContext->debugVertexNormal = settings.debugVertexNormal;
-        engineContext->debugFaceNormal = settings.debugFaceNormal;
-        engineContext->debugBone = settings.debugBone;
-
-        if (!settings.shaderCacheKey.IsEmpty())
-    }
+    // Hazel::FileMemory settingsFile(_settingsFilePath.c_str(),
+    //                                Hazel::FileOpenMode::OPEN);
+    // LvInputStream inputStream(&settingsFile);
+    // 
+    // const std::string settingJsonString = inputStream.ReadToEnd();
+    // 
+    // settingsFile.FlushToFile();
+    // settingsFile.End();
+    // 
+    // ProjectContext *project = BJ_GetProjectContext();
+    // 
+    // LvProjectSettings &settings = project->settings;
+    // 
+    // Hazel::JsonSerializer archive(settingJsonString.c_str());
+    // archive.Deserialize(Hazel::Reflection::GetTypeID<LvProjectSettings>(),
+    //                     &settings);
+    // 
+    // LvEngineContext *engineContext = lv_engine_get_context();
+    // engineContext->sceneGraphPath = settings.defaultSceneGraphFileName;
+    // 
+    // if (settings.windowLayoutFilePath.empty())
+    // {
+    //     settings.windowLayoutFilePath =
+    //         LvPanelController::defaultDockingLayoutPath;
+    // }
+    // 
+    // engineContext->isGPUskinning = settings.isGPUskinning;
+    // if (settings.useHDR)
+    // {
+    //     engineContext->colorRenderTextureFormat =
+    //         Render::LvPixelFormat::R16G16B16A16_SFLOAT;
+    // }
+    // else
+    // {
+    //     engineContext->colorRenderTextureFormat =
+    //         Render::LvPixelFormat::R8G8B8A8_UNORM;
+    // }
+    // engineContext->debugWireFrame = settings.debugWireFrame;
+    // engineContext->debugVertexNormal = settings.debugVertexNormal;
+    // engineContext->debugFaceNormal = settings.debugFaceNormal;
+    // engineContext->debugBone = settings.debugBone;
+    // 
+    // if (!settings.shaderCacheKey.IsEmpty())
+    // {
+    // 
+    // }
+    // 
 
     } // namespace HazelEditor
